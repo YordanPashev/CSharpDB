c4b4b0358a3f553031ea41eb411120052a3defbd
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-09 22:09:54 +0300

    Adding queries to T04-09

--- MS SQL/05.Subqueries and Joins/Subqueries and Joins.sql
+++ MS SQL/05.Subqueries and Joins/Subqueries and Joins.sql
@@ -32,3 +32,67 @@ ORDER BY [FirstName]
     JOIN [Departments] AS d ON d.[DepartmentID] = e.[DepartmentID]
    WHERE d.[Name] = 'Sales'
 ORDER BY [EmployeeID]
+
+--4--
+
+  SELECT TOP (5) 
+         e.[EmployeeID]
+		 , e.[FirstName]
+		 , e.[Salary]
+		 , d.[Name] AS 'DepartmentName'
+    FROM [Employees] AS e
+    JOIN [Departments] AS d ON d.[DepartmentID] = e.[DepartmentID]
+   WHERE e.[Salary] > 15000
+ORDER BY d.[DepartmentID]
+
+--5--
+
+    SELECT TOP (3) e.[EmployeeID]
+          ,e.[Firstname]FROM [Employees] AS e
+LEFT JOIN [EmployeesProjects] AS ep ON ep.[EmployeeID] = e.[EmployeeID]
+    WHERE ep.[EmployeeID] IS NULL
+ ORDER BY ep.[EmployeeID]
+
+ --6--
+
+    SELECT 
+	       e.[FirstName]
+		   , e.[Lastname]
+		   , e.[HireDate]
+		   , d.[Name] AS [DeptName]
+      FROM [Departments] AS d
+	  JOIN [Employees] AS e 
+	    ON d.[DepartmentID] = e.[DepartmentID]
+	 WHERE e.[HireDate] > '1999-01-01'
+	   AND d.[Name] IN ('Sales', 'Finance') 
+  ORDER BY e.[HireDate]
+
+--7--
+
+  SELECT TOP (5)
+		 ep.[EmployeeID]
+		 , e.[FirstName]
+		 , p.[Name] AS [ProjectName]
+    FROM [EmployeesProjects] AS ep
+	JOIN [Employees] AS e
+      ON e.[EmployeeID] = ep.[EmployeeID]
+    JOIN [Projects] AS p
+      ON p.[ProjectID] = ep.[ProjectID] 
+   WHERE p.[StartDate] > '2002-08-13' 
+     AND [EndDate] IS NULL
+ORDER BY [EmployeeID]
+
+--8--
+
+  SELECT 
+		  ep.[EmployeeID]
+		  , e.[FirstName]
+		  , [Name] AS [Project Name]
+     FROM [Employees] AS e
+	 JOIN [EmployeesProjects] AS ep
+       ON e.[EmployeeID] = ep.[EmployeeID]
+LEFT JOIN [Projects] AS p
+       ON p.[ProjectID] = ep.[ProjectID]
+	  AND p.[StartDate] < '2005.01.01'
+    WHERE ep.[EmployeeID] = 24 
+    
