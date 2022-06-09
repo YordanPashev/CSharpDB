3af2d0ed5ae59425f2778cd55ae166686e78447d
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-09 14:05:52 +0300

    Adding queries to T18 and 19

--- MS SQL/04.Built-in Functions/Built-in Functions.sql
+++ MS SQL/04.Built-in Functions/Built-in Functions.sql
@@ -132,7 +132,7 @@ ORDER BY [Start]
 
   SELECT 
          [UserName]
-	     , SUBSTRING([Email], CHARINDEX('@', [Email]) + 1, LEN([Email]) - CHARINDEX('@', [Email])) AS [Email Provider]
+	     , SUBSTRING([Email], CHARINDEX('@', [Email]) + 1, LEN([Email])) AS [Email Provider]
     FROM [Users]
 ORDER BY [Email Provider]
 		 , [UserName]
@@ -164,3 +164,34 @@ ORDER BY [UserName]
 	ORDER BY [Name]
 	         , [Duration] 
 			 , [Part of the Day]
+
+--18--
+
+ SELECT [ProductName]
+	    , [OrderDate]
+	    , DATEADD(DAY, 3, [OrderDate]) AS [Pay Due]
+	    , DATEADD(MONTH, 1, [OrderDate]) AS [Deliver Due]
+   FROM [Orders]
+
+--19--
+
+CREATE TABLE [People](
+             [Id] INT PRIMARY KEY IDENTITY
+			 , [Name] NVARCHAR (50)
+			 , [Birthdate] DATETIME2
+)
+
+ INSERT INTO [People]([Name], [Birthdate]) 
+      VALUES
+			 ('Victor', '2000-12-07 00:00:00.000')
+			 , ('Steven', '1992-09-10 00:00:00.000')
+			 , ('Stephen', '1910-09-19 00:00:00.000')
+			 , ('John', '2010-01-06 00:00:00.000')
+
+      SELECT [Name]
+	         , DATEDIFF(YEAR, [Birthdate], GETDATE()) AS [Age in Years]
+	         , DATEDIFF(MONTH, [Birthdate], GETDATE()) AS [Age in Months]
+	         , DATEDIFF(DAY, [Birthdate], GETDATE()) AS [Age in Days]
+	         , DATEDIFF(MINUTE, [Birthdate], GETDATE()) AS [Age in Minutes]
+	    FROM [People]
+
