56848c572863b1435e199e3ed869137639307516
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-09 12:28:48 +0300

    Adding queries to T14 - 17

--- MS SQL/04.Built-in Functions/Built-in Functions.sql
+++ MS SQL/04.Built-in Functions/Built-in Functions.sql
@@ -119,3 +119,48 @@ ORDER BY [IsoCode]
 ORDER BY [Mix]
 
 --14--
+ 
+  SELECT TOP (50)
+	     [Name]
+	     , FORMAT([Start], 'yyyy-MM-dd') AS [Start]
+    FROM [Games]
+   WHERE DATEPART(YEAR, [Start]) IN ('2011', '2012')
+ORDER BY [Start]
+		 ,[Name]
+
+--15--
+
+  SELECT 
+         [UserName]
+	     , SUBSTRING([Email], CHARINDEX('@', [Email]) + 1, LEN([Email]) - CHARINDEX('@', [Email])) AS [Email Provider]
+    FROM [Users]
+ORDER BY [Email Provider]
+		 , [UserName]
+
+--16--
+
+  SELECT 
+         [UserName]
+		 , [IpAddress]
+    FROM [Users]
+   WHERE [IpAddress] LIKE '___.1%.%.___'
+ORDER BY [UserName]
+
+--17--
+
+      SELECT [Name]   
+			 ,CASE
+				 WHEN DATEPART(HOUR, [Start]) BETWEEN 0 AND 11 THEN 'Morning'
+				 WHEN DATEPART(HOUR, [Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+				 WHEN DATEPART(HOUR, [Start]) BETWEEN 18 AND 23 THEN 'Evening'
+			 END AS [Part of the Day]
+	         ,CASE 
+	         	 WHEN [Duration] BETWEEN 0 AND 3 THEN 'Extra Short'
+	         	 WHEN [Duration] BETWEEN 4 AND 6 THEN 'Short'
+	         	 WHEN [Duration] > 6 THEN 'Long'
+	         	 ELSE 'Extra Long'
+	         END AS [Duration]
+        FROM [Games]
+	ORDER BY [Name]
+	         , [Duration] 
+			 , [Part of the Day]
