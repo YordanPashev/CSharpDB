f18a54e1a25a82d0f182c519f325604c66d3be62
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-06-13 13:40:59 +0300

    Adding All Tasks Description and queries to T01, 02, 03 and 05

---
+++ MS SQL/07.Functions, Procedures, Triggers and Transactions - Exercise/Functions, Procedures, Triggers and Transactions.sql
@@ -0,0 +1,51 @@
+--1--
+
+CREATE PROC usp_GetEmployeesSalaryAbove35000
+         AS 
+	 SELECT e.[FirstName]
+			, e.[LastName]
+	   FROM [Employees] AS e
+	  WHERE e.[Salary] > 35000
+--2--
+
+CREATE PROC usp_GetEmployeesSalaryAboveNumber(@Salary DECIMAL(18,4))
+         AS 
+	 SELECT e.[FirstName]
+			, e.[LastName]
+	   FROM [Employees] AS e
+	  WHERE e.[Salary] >= @Salary
+
+--3--
+
+CREATE PROC usp_GetTownsStartingWith(@string NVARCHAR(20))
+         AS 
+	 SELECT t.[Name]
+	   FROM [Towns] AS t
+	  WHERE t.[Name] LIKE @string + '%'
+
+	  EXEC usp_GetTownsStartingWith 'b'
+--5--
+
+CREATE FUNCTION ufn_GetSalaryLevel (@salary DECIMAL(18,4))
+RETURNS NVARCHAR(10)
+AS
+BEGIN
+	  DECLARE @result NVARCHAR(10);
+	  IF(@salary < 30000)
+	  BEGIN
+		SET @result = 'Low'
+	  END
+	  IF(@salary BETWEEN 30000 AND 50000)
+	  BEGIN
+		SET @result = 'Average'
+	  END
+	  IF(@salary > 50000)
+	  BEGIN
+		SET @result = 'High'
+	  END
+	  RETURN @result
+END
+
+SELECT e.[Salary]
+       , dbo.ufn_GetSalaryLevel(e.[Salary])
+  FROM [Employees] AS e
